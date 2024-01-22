0ee89339997d7f2bb8967badb48e89c086c75ba2
dwerkjem <derekrneilson@gmail.com>
2024-01-22 04:21:43 -0700

    Refactor internet connection check and API key validation in main.py

--- src/main.py
+++ src/main.py
@@ -10,17 +10,16 @@ root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__).split("src"
 
 def main():
     # check for internet connection
-    try:
-        testResponse = requests.get("https://google.com")
-        assert testResponse.status_code == 200
-        print("Internet connection found")
-    except:
-        print("No internet connection")
-        sys.exit(1)
+    testResponse = requests.get("https://google.com")
+    assert testResponse.status_code == 200 , "No internet"
+    print("Internet connection found")
     # load environment variables
     load_dotenv(dotenv_path=root_dir + "/.env")
-    # check for api keys
-    
+    maps_apikey = load_dotenv('GOOGLE-MAPS-API-KEY')
+    api_response = requests.post('https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810%2C-119.6822510&timestamp=1331161200&key={maps_apikey}')
+    # assert api_response is status code 200
+    assert api_response.status_code == 200, "API request failed"
+    print("API key valid")
 
 if __name__ == "__main__":
     main()
